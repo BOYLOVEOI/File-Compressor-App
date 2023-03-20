100e49e7aa39e6aa7b1a715f9cb261029258e922
Paul Lavoie <paullavoie2332@gmail.com>
2023-03-20 04:10:39 -0400

    make_archive further edited and fixed

--- zip_creator.py
+++ zip_creator.py
@@ -13,5 +13,10 @@ def make_archive(filepath, directory):
                          compression=zipfile.ZIP_DEFLATED, 
                          # Compresslevel set to 9, but can be changed depending upon user
                          compresslevel=9) as zf:
-        for file in filepath:   
-            zf.write(file)
+        
+        for file in filepath:
+            # To get the arcnames, or specify the file names that we want to zip, we need another pathlib object
+            file = pathlib.Path(file)   
+            
+            # file.name gives us the name of the file in the path 
+            zf.write(file, arcname=file.name)
