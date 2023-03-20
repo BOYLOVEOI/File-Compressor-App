b068556dfbe4b0dee23012b5462ac13e1e92f1cd
Paul Lavoie <paullavoie2332@gmail.com>
2023-03-20 04:11:56 -0400

    While loop implemented. Exit button + theme added

--- gui.py
+++ gui.py
@@ -1,25 +1,50 @@
 import PySimpleGUI as sg
+import zip_creator as zc
 
 # Variable Declaration
 label1 = sg.Text('Select Files to Compress:')
 input1 = sg.Input()
 
 # FilesBrowse() is a button that allows for the selection of files
-button1 = sg.FilesBrowse('Choose') 
+button1 = sg.FilesBrowse('Choose', key='-FILEPATH-') 
 
 # FolderBrowse() is a button that allows for the selection of folders
 label2 = sg.Text('Select Destination Folder:')
 input2 = sg.Input()
-button2 = sg.FolderBrowse('Choose')
+button2 = sg.FolderBrowse('Choose', key='-DIRECTORY-')
 
+# Compress button
 compress = sg.Button('Compress')
 
-# Window Creation
-window = sg.Window('File Compressor', layout=[[
-                                            [label1, input1, button1], 
-                                            [label2, input2, button2],
-                                            [compress]]])
+# Exit button
+exit = sg.Button('Exit')
 
-# Calling window.read() to create the window                                       
-window.read()
+# Theme
+sg.theme(new_theme='DarkGrey4')
+
+# Window Creation
+window = sg.Window('File Compressor', layout=[[label1, input1, button1], 
+                                              [label2, input2, button2],
+                                              [compress, exit]],
+                                      font=('Helvetica', 10))
+
+while True:
+# Calling window.read() to create the window  
+# Assigning the values of window.read() to variables, event and values                                     
+    event, values = window.read()
+
+    print(event)
+    print(values)
+
+    # Variable Declarations
+    filepath = values['-FILEPATH-'].split(';')
+    destination = values['-DIRECTORY-']
+
+    # If block to compress files or exit application
+    if event == 'Compress':
+        zc.make_archive(filepath, destination)
+    
+    elif event == sg.WINDOW_CLOSED or event == 'Exit':
+        break
+    
 window.close()
